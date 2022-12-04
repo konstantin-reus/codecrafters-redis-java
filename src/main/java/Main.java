c18ee114d42d9089ac935ef462fdf8ba58f6bc6a
Konstantin Reus <kr5031@gmail.com>
2022-12-05 00:59:11 +0100

    Stage 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,30 @@ import java.net.*;
 
 public class Main {
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        System.out.println("Logs from your program will appear here!");
-        try (ServerSocket serverSocket = new ServerSocket(6379);
-             Socket clientSocket = serverSocket.accept()) {
+        int messages = 1;
+        int incomingConnections = 0;
+
+        try (ServerSocket serverSocket = new ServerSocket(6379)) {
             serverSocket.setReuseAddress(true);
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            handleIncomingConnection(messages, incomingConnections, serverSocket);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void handleIncomingConnection(int messages,
+                                                 int incomingConnections,
+                                                 ServerSocket serverSocket) {
+        try (Socket clientSocket = serverSocket.accept()) {
+            System.out.println("Got incoming connection #" + ++incomingConnections);
+            InputStream in = clientSocket.getInputStream();
+            byte[] bytes = new byte[1024];
+            while (in.read(bytes) != -1) {
+                System.out.println("Received message #" + messages + ": " + new String(bytes, 0, bytes.length));
+                clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+                System.out.println("Send PONG #" + messages);
+                messages++;
+            }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
