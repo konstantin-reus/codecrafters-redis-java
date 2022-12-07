5799c289b133c0a7512e6436765f588ac96f86c4
Konstantin Reus <kr5031@gmail.com>
2022-12-07 18:50:29 +0100

    Stage 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,37 @@
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args) {
-        int messages = 1;
-        int incomingConnections = 0;
+    public static void main(String[] args) throws IOException {
+        int messages = 0;
+        int cpuCores = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(cpuCores);
 
         try (ServerSocket serverSocket = new ServerSocket(6379)) {
             serverSocket.setReuseAddress(true);
-            handleIncomingConnection(messages, incomingConnections, serverSocket);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                executor.submit(() -> {
+                    try {
+                        handleIncomingConnection(messages, clientSocket);
+                    } catch (IOException e) {
+                    }
+                });
+            }
         }
     }
 
     private static void handleIncomingConnection(int messages,
-                                                 int incomingConnections,
-                                                 ServerSocket serverSocket) {
-        try (Socket clientSocket = serverSocket.accept()) {
-            System.out.println("Got incoming connection #" + ++incomingConnections);
-            InputStream in = clientSocket.getInputStream();
-            byte[] bytes = new byte[1024];
-            while (in.read(bytes) != -1) {
-                System.out.println("Received message #" + messages + ": " + new String(bytes, 0, bytes.length));
-                clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-                System.out.println("Send PONG #" + messages);
-                messages++;
-            }
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
+                                                 Socket clientSocket) throws IOException {
+        InputStream in = clientSocket.getInputStream();
+        byte[] bytes = new byte[1024];
+        while (in.read(bytes) != -1) {
+            System.out.println("[" + Thread.currentThread() + "] Received message #" + messages + ": " + new String(bytes, 0, bytes.length));
+            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            System.out.println("[" + Thread.currentThread() + "] Send PONG #" + messages);
+            messages++;
         }
+        clientSocket.close();
     }
 }
